f7ba500a87d45ecdce1d420eea3c3efccd3d080d
阎王爷贴告示 <975942219@qq.com>
2022-04-13 16:58:30 +0800

    添加类Global

---
+++ Hu.Api/Extensions/Global.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hu.Api.Extensions
+{
+    /// <summary>
+    /// 全局变量
+    /// </summary>
+    public class Global
+    {
+        /// <summary>
+        /// 部署到Azure，服务器时间都是UTC，需要 .AddHours(8) 变成本地时间
+        /// </summary>
+        public static DateTime CurrentTime => DateTime.Now.AddHours(8);
+    }
+}
